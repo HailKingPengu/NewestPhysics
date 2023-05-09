972525a45873d4b98ce2fbfde9fd23e4258d7426
KingPengu <rutger2003@gmail.com>
2023-05-09 22:18:03 +0200

    Added basic scene and button functionality

--- GXPEngine/MyGame.cs
+++ GXPEngine/MyGame.cs
@@ -1,32 +1,56 @@
 using System;                                   // System contains a lot of default C# libraries 
 using GXPEngine;                                // GXPEngine contains the engine
 using System.Drawing;                           // System.Drawing contains drawing tools such as Color definitions
+using System.Collections.Generic;
+using GXPEngine.Scenes;
 
 public class MyGame : Game
 {
-    EasyDraw canvas;
+
+    List<Scene> scenes;
+
+    int currentScene = 0;
+
+
 
     public MyGame() : base(800, 600, false)
-    {     // Create a window that's 800x600 and NOT fullscreen
+    {
+
+        scenes = new List<Scene>();
+
+        scenes.Add(new Scene());
+        scenes[0].AddButton(new Button(new Vec2(400, 300), new Vec2(200, 50), Color.White, "gaming", 0, 0));
+        scenes[0].AddButton(new Button(new Vec2(150, 300), new Vec2(200, 50), Color.White, "gaming", 0, 0));
+        scenes[0].AddButton(new Button(new Vec2(650, 300), new Vec2(200, 50), Color.White, "gaming", 0, 0));
+
+        scenes[0].AddButton(new Button(new Vec2(400, 150), new Vec2(200, 50), Color.White, "gaming", 0, 0));
+        scenes[0].AddButton(new Button(new Vec2(150, 150), new Vec2(200, 50), Color.White, "gaming", 0, 0));
+        scenes[0].AddButton(new Button(new Vec2(650, 150), new Vec2(200, 50), Color.White, "gaming", 0, 0));
+
+        scenes[0].AddButton(new Button(new Vec2(400, 450), new Vec2(200, 50), Color.White, "gaming", 0, 0));
+        scenes[0].AddButton(new Button(new Vec2(150, 450), new Vec2(200, 50), Color.White, "gaming", 0, 0));
+        scenes[0].AddButton(new Button(new Vec2(650, 450), new Vec2(200, 50), Color.White, "gaming", 0, 0));
+
+        scenes[0].AddButton(new Button(new Vec2(400, 225), new Vec2(200, 50), Color.White, "gaming", 0, 0));
+        scenes[0].AddButton(new Button(new Vec2(150, 225), new Vec2(200, 50), Color.White, "gaming", 0, 0));
+        scenes[0].AddButton(new Button(new Vec2(650, 225), new Vec2(200, 50), Color.White, "gaming", 0, 0));
+
+        scenes[0].AddButton(new Button(new Vec2(400, 375), new Vec2(200, 50), Color.White, "gaming", 0, 0));
+        scenes[0].AddButton(new Button(new Vec2(150, 375), new Vec2(200, 50), Color.White, "gaming", 0, 0));
+        scenes[0].AddButton(new Button(new Vec2(650, 375), new Vec2(200, 50), Color.White, "gaming", 0, 0));
+
+        AddChild(scenes[currentScene]);
 
 
-        AddChild(new AABB(0.1f, 10, new Vec2(100, 90), new Vec2(10, 10)));
-        AddChild(new AABB(0.2f, 20, new Vec2(100, 150), new Vec2(20,20)));
-
-        // Add the canvas to the engine to display it:
-        //AddChild(canvas);
-        Console.WriteLine("MyGame initialized");
     }
 
     void Update()
     {
-        //canvas.Clear(Color.Black);
 
     }
 
     static void Main()
     {
-
         new MyGame().Start();
     }
 }
