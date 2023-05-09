6a91d3254ae4cb246afa2370119155196a31eeae
KingPengu <rutger2003@gmail.com>
2023-05-09 22:17:03 +0200

    Added basic scene and button functionality, removed boids file

--- GXPEngine/MyGame.cs
+++ GXPEngine/MyGame.cs
@@ -9,15 +9,6 @@ public class MyGame : Game
     public MyGame() : base(800, 600, false)
     {     // Create a window that's 800x600 and NOT fullscreen
 
-        // Draw some things on a canvas:
-        canvas = new EasyDraw(1920, 1080);
-        canvas.Clear(Color.MediumPurple);
-        canvas.Fill(Color.Yellow);
-        canvas.Ellipse(width / 2, height / 2, 200, 200);
-        canvas.Fill(50);
-        canvas.TextSize(32);
-        canvas.TextAlign(CenterMode.Center, CenterMode.Center);
-        canvas.Text("Welcome!", width / 2, height / 2);
 
         AddChild(new AABB(0.1f, 10, new Vec2(100, 90), new Vec2(10, 10)));
         AddChild(new AABB(0.2f, 20, new Vec2(100, 150), new Vec2(20,20)));
@@ -27,14 +18,13 @@ public class MyGame : Game
         Console.WriteLine("MyGame initialized");
     }
 
-    // For every game object, Update is called every frame, by the engine:
     void Update()
     {
         //canvas.Clear(Color.Black);
 
     }
 
-    static void Main()                          // Main() is the first method that's called when the program is run
+    static void Main()
     {
 
         new MyGame().Start();
