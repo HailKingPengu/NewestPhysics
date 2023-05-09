908da2b149ae0c2b4cc148845e2274529114b367
KingPengu <rutger2003@gmail.com>
2023-05-09 13:28:33 +0200

    Added HeatCollider and HeatComponent

---
+++ GXPEngine/Fire/Heat/HeatCollider.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GXPEngine.Fire
+{
+    public class HeatCollider : Collider
+    {
+
+        GameObject owner;
+        public HeatComponent heat;
+
+        Vec2 size;
+        Vec2 center;
+
+        Vec2 max;
+        Vec2 min;
+
+        public HeatCollider(GameObject owner, HeatComponent heatComponent, params string[] args) : base(owner, args)
+        {
+            heat = heatComponent;
+
+            max = center + size;
+            min = center - size;
+        }
+
+        public void Collide(List<HeatCollider> colliders)
+        {
+            for(int i = 0; i < colliders.Count; i++)
+            {
+                if (colliders[i].max.x <= min.x && colliders[i].min.x <= max.x &&
+                    colliders[i].max.y <= min.y && colliders[i].min.y <= max.y)
+                {
+                    float otherHeat = colliders[i].heat.currentHeat;
+                    //replace with reference to HeatComponent
+                    if (otherHeat < heat.currentHeat)
+                    {
+                        float deltaHeat = heat.currentHeat - otherHeat;
+                        otherHeat += 0.1f * deltaHeat;
+                        heat.currentHeat -= 0.05f * deltaHeat;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ GXPEngine/Fire/Heat/HeatComponent.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GXPEngine.Fire
+{
+    public class HeatComponent : Component
+    {
+
+        HeatCollider colliderChild;
+
+        public float currentHeat;
+        float burnThreshold;
+
+        bool burning;
+
+        public HeatComponent(GameObject owner, float materialThreshold) : base(owner)
+        {
+
+            colliderChild = new HeatCollider(owner, this, new string[1]);
+
+            if (owner.Collider is PolygonCollider)
+            {
+                burnThreshold = materialThreshold * FindPolygonSurf(owner.Collider as PolygonCollider);
+            }
+            else if(owner.Collider is CircleCollider) 
+            {
+                burnThreshold = materialThreshold * ((owner.Collider as CircleCollider).Radius * (owner.Collider as CircleCollider).Radius * Mathf.PI);
+            }
+
+        }
+
+        new void Update()
+        {
+            if (burning)
+            {
+                //list of all HeatColliders
+                colliderChild.Collide(new List<HeatCollider>());
+            }
+        }
+
+
+        float FindPolygonSurf(PolygonCollider collider)
+        {
+            Vec2[] points = collider.Points;
+
+            float minx = Mathf.Infinity;
+            float miny = Mathf.Infinity;
+            float maxx = -Mathf.Infinity;
+            float maxy = -Mathf.Infinity;
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                if (points[i].x < minx) minx = points[i].x;
+                if (points[i].y < miny) miny = points[i].y;
+                if (points[i].x > maxx) maxx = points[i].x;
+                if (points[i].y > maxy) maxy = points[i].y;
+            }
+
+            return (maxx - minx) * (maxy - maxx);
+
+        }
+
+
+    }
+}
