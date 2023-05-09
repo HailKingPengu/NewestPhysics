7f2917082d714b943af37e0cb91f00c95d965905
TheHunter7357 <55022262+TheHunter7357@users.noreply.github.com>
2023-05-09 13:22:52 +0200

    added fire and removed boids

--- GXPEngine/MyGame.cs
+++ GXPEngine/MyGame.cs
@@ -6,55 +6,6 @@ public class Boids : Game
 {
     EasyDraw canvas;
 
-    int numBoids = 250;
-
-    Boid[] boids;
-
-    float boidSpeed = 3;
-    float[] boidX;
-    float[] boidY;
-    float[] boidRot;
-    float[] boidMoveX;
-    float[] boidMoveY;
-
-    int screenSizeX = 1920;
-    int screenSizeY = 1080;
-
-    float distThresholdAlign = 10;
-    float distThresholdSepar = 10;
-    float distThresholdCohes = 15;
-    float dist;
-    float closestDist;
-    int closestI;
-    int[] isClose;
-
-    int friends;
-
-    float normFac;
-
-    float closeBoidMoveX;
-    float closeBoidMoveY;
-    float closeBoidMoveXNorm;
-    float closeBoidMoveYNorm;
-
-    float difX;
-    float difY;
-    float difXNorm;
-    float difYNorm;
-
-    float closeBoidX;
-    float closeBoidY;
-    float closeBoidXNorm;
-    float closeBoidYNorm;
-
-    //control values
-    float alignment = 0.015f;
-    float separation = 0.008f;
-    float cohesion = 0.0003f;
-
-    float correctionForce = 0.3f;
-
-
     public Boids() : base(1920, 1080, true)
     {     // Create a window that's 800x600 and NOT fullscreen
 
@@ -68,13 +19,6 @@ public class Boids : Game
         canvas.TextAlign(CenterMode.Center, CenterMode.Center);
         canvas.Text("Welcome!", width / 2, height / 2);
 
-        boids = new Boid[numBoids];
-
-        for (int i = 0; i < numBoids; i++)
-        {
-            boids[i] = new Boid(numBoids, distThresholdSepar, separation, distThresholdAlign, alignment, distThresholdCohes, cohesion, i, screenSizeX, screenSizeY);
-        }
-
         // Add the canvas to the engine to display it:
         AddChild(canvas);
         Console.WriteLine("MyGame initialized");
@@ -83,15 +27,8 @@ public class Boids : Game
     // For every game object, Update is called every frame, by the engine:
     void Update()
     {
-
         canvas.Clear(Color.Black);
 
-        for (int i = 0; i < numBoids; i++)
-        {
-            float[] boidCoords = boids[i].updateBoid(boids);
-            canvas.Ellipse(boidCoords[0],boidCoords[1],5,5);
-
-        }
     }
 
     static void Main()                          // Main() is the first method that's called when the program is run
