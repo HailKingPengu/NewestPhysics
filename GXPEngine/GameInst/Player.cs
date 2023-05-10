64226da84fd8e87d9ecce4d5ff99cf23e18215a4
KingPengu <rutger2003@gmail.com>
2023-05-10 12:51:03 +0200

    ...

--- GXPEngine/GameInst/Player.cs
+++ GXPEngine/GameInst/Player.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GXPEngine.GameInstance
+namespace GXPEngine.GameInst
 {
     internal class Player : AnimationSprite
     {
