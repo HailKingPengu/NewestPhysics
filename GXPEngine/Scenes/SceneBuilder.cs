454685025b72f3ed92e6c52793f156b6efc6386f
KingPengu <rutger2003@gmail.com>
2023-05-11 10:07:23 +0200

    Merging changes

--- GXPEngine/Scenes/SceneBuilder.cs
+++ GXPEngine/Scenes/SceneBuilder.cs
@@ -17,13 +17,13 @@ namespace GXPEngine.Scenes
             scenes[0].AddButton(new Button(new Vec2(400, 300), new Vec2(200, 50), Color.White, "gaming", 0, 1, sceneManager));
             scenes[0].AddButton(new Button(new Vec2(400, 380), new Vec2(200, 50), Color.White, "settings", 0, 2, sceneManager));
             scenes[0].AddButton(new Button(new Vec2(400, 460), new Vec2(200, 50), Color.White, "exit", 1, 0, sceneManager));
-            scenes[0].AddBackground(new Sprite("Assets/background1.png"), screenSize);
+            scenes[0].AddBackground(new Sprite("../../Assets/Background1.png"), screenSize);
             scenes[0].isActive = true;
 
             //game
             scenes.Add(new Scene());
             scenes[1].AddButton(new Button(new Vec2(400, 300), new Vec2(200, 50), Color.White, "WAAAAA", 0, 0, sceneManager));
-            scenes[1].AddBackground(new Sprite("Assets/background2.jpg"), screenSize);
+            scenes[1].AddBackground(new Sprite("../../Assets/Background2.jpg"), screenSize);
             scenes[1].AddGame(new GameInstance());
             scenes[1].isActive = true;
 
@@ -32,7 +32,7 @@ namespace GXPEngine.Scenes
             scenes[2].AddButton(new Button(new Vec2(400, 300), new Vec2(200, 50), Color.White, "back", 0, 0, sceneManager));
             scenes[2].AddButton(new Button(new Vec2(400, 380), new Vec2(200, 50), Color.White, "sound idk", 0, 2, sceneManager));
             scenes[2].AddButton(new Button(new Vec2(400, 460), new Vec2(200, 50), Color.White, "bomb the nurburgring", 0, 2, sceneManager));
-            scenes[2].AddBackground(new Sprite("Assets/background3.gif"), screenSize);
+            scenes[2].AddBackground(new Sprite("../../Assets/Background3.gif"), screenSize);
             scenes[2].isActive = true;
         }
 
